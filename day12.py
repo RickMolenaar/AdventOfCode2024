6bb94da2cc5d669e74341d189d081e0eac01fd6c
Rick Molenaar <rick_molenaar96@live.nl>
2024-12-12 16:13:59 +0100

    Day 12 easier edge checking

--- day12.py
+++ day12.py
@@ -15,7 +15,7 @@ def format_input(inp: list[str]):
             grid[x, y] = c
     return grid
 
-def get_area(grid, loc, area = None, example = False):
+def get_area(grid, loc, area = None):
     if area is None:
         area = set([loc])
     plant = grid[loc]
@@ -26,77 +26,44 @@ def get_area(grid, loc, area = None, example = False):
         try:
             if grid[nx, ny] == plant:
                 area.add((nx, ny))
-                area.update(get_area(grid, (nx, ny), area, example))
+                area.update(get_area(grid, (nx, ny), area))
         except KeyError:
             continue
     return area
 
 def get_price(grid, area: set, price_type):
-    borders = []
+    edges = set()
     for loc in area:
         plant = grid[loc]
-        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            nx, ny = loc[0] + dx, loc[1] + dy
-            try:
-                if grid[nx, ny] != grid[loc]:
-                    borders.append((nx, ny))
-            except KeyError:
-                borders.append((nx, ny))
+        for dx, dy, dir in [(-1, 0, 'l'), (1, 0, 'r'), (0, -1, 'd'), (0, 1, 'u')]:
+            neighbor = (loc[0] + dx, loc[1] + dy)
+            if neighbor not in grid or grid[neighbor] != grid[loc]:
+                edges.add((*loc, dir))
     if price_type == 'perimeter':
-        return len(area) * len(borders)
+        return len(area) * len(edges)
     
     sides = 0
-    checked = set()
-    for (x, y) in sorted(area):
-        if (x, y, 'u') not in checked:
-            if (x, y-1) in borders:
-                sides += 1
-                checked.add((x, y, 'u'))
-                dx = -1
-                while (x + dx, y) not in borders and (x + dx, y - 1) in borders:
-                    checked.add((x + dx, y, 'u'))
-                    dx -= 1
-                dx = 1
-                while (x + dx, y) not in borders and (x + dx, y - 1) in borders:
-                    checked.add((x + dx, y, 'u'))
-                    dx += 1
-        if (x, y, 'd') not in checked:
-            if (x, y+1) in borders:
-                sides += 1
-                checked.add((x, y, 'd'))
-                dx = -1
-                while (x + dx, y) not in borders and (x + dx, y + 1) in borders:
-                    checked.add((x + dx, y, 'd'))
-                    dx -= 1
-                dx = 1
-                while (x + dx, y) not in borders and (x + dx, y + 1) in borders:
-                    checked.add((x + dx, y, 'd'))
-                    dx += 1
-        if (x, y, 'l') not in checked:
-            if (x - 1, y) in borders:
-                sides += 1
-                checked.add((x, y, 'l'))
-                dy = -1
-                while (x, y + dy) not in borders and (x - 1, y + dy) in borders:
-                    checked.add((x, y + dy, 'l'))
-                    dy -= 1
-                dy = 1
-                while (x, y + dy) not in borders and (x - 1, y + dy) in borders:
-                    checked.add((x, y + dy, 'l'))
-                    dy += 1
-        if (x, y, 'r') not in checked:
-            if (x + 1, y) in borders:
-                sides += 1
-                checked.add((x, y, 'r'))
-                dy = -1
-                while (x, y + dy) not in borders and (x + 1, y + dy) in borders:
-                    checked.add((x, y + dy, 'r'))
-                    dy -= 1
-                dy = 1
-                while (x, y + dy) not in borders and (x + 1, y + dy) in borders:
-                    checked.add((x, y + dy, 'r'))
-                    dy += 1
-    
+    while edges:
+        x, y, dir = edges.pop()
+        sides += 1
+        if dir in 'ud':
+            dx = -1
+            while (x + dx, y, dir) in edges:
+                edges.remove((x + dx, y, dir))
+                dx -= 1
+            dx = 1
+            while (x + dx, y, dir) in edges:
+                edges.remove((x + dx, y, dir))
+                dx += 1
+        else:
+            dy = -1
+            while (x, y + dy, dir) in edges:
+                edges.remove((x, y + dy, dir))
+                dy -= 1
+            dy = 1
+            while (x, y + dy, dir) in edges:
+                edges.remove((x, y + dy, dir))
+                dy += 1
     return len(area) * sides
     
 
@@ -107,7 +74,7 @@ def solve(grid, part, example):
         for x in range(max(grid)[0] + 1):
             if (x, y) in seen:
                 continue
-            area = get_area(grid, (x, y), example = example)
+            area = get_area(grid, (x, y))
             seen.update(set(area))
             s += get_price(grid, area, 'perimeter' if part == 1 else 'sides')
     return s
